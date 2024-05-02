3c9c27ac55d93568389b3f2daba883c8ddaca6b4
Ricky Zhang <16908811+rickyzhangca@users.noreply.github.com>
2024-05-02 17:05:02 -0400

    support code highlighting

--- src/plugins/rehype/rehypeSyntaxHighlighting.ts
+++ src/plugins/rehype/rehypeSyntaxHighlighting.ts
@@ -1,7 +1,9 @@
-import { Node, toString } from "hast-util-to-string";
-import { refractor } from "refractor/lib/all.js";
-import { visit } from "unist-util-visit";
-import { Parent } from "unist";
+import { Node, toString } from 'hast-util-to-string';
+import rangeParser from 'parse-numeric-range';
+import { Text } from 'refractor';
+import { RefractorElement, refractor } from 'refractor/lib/all.js';
+import { Parent } from 'unist';
+import { visit } from 'unist-util-visit';
 
 export const rehypeSyntaxHighlighting = (options: {
   ignoreMissing?: boolean;
@@ -14,7 +16,7 @@ export const rehypeSyntaxHighlighting = (options: {
   return (tree: Parent) => {
     visit(
       tree,
-      "element",
+      'element',
       (
         node: Node & {
           tagName: string;
@@ -31,7 +33,7 @@ export const rehypeSyntaxHighlighting = (options: {
           };
         }
       ) => {
-        if (!parent || parent.tagName !== "pre" || node.tagName !== "code") {
+        if (!parent || parent.tagName !== 'pre' || node.tagName !== 'code') {
           return;
         }
 
@@ -43,16 +45,13 @@ export const rehypeSyntaxHighlighting = (options: {
 
         let result;
         try {
-          parent.properties.className = (
-            parent.properties.className || []
-          ).concat("language-" + lang);
-          result = refractor.highlight(toString(node), lang);
-          node.children = result.children;
+          parent.properties.className = (parent.properties.className || []).concat(
+            'language-' + lang
+          );
+          result = highlight(node, lang);
+          node.children = result;
         } catch (err) {
-          if (
-            options.ignoreMissing &&
-            /Unknown language/.test((err as Error).message)
-          ) {
+          if (options.ignoreMissing && /Unknown language/.test((err as Error).message)) {
             return;
           }
           throw err;
@@ -74,10 +73,60 @@ function getLanguage(
   const className = node.properties.className || [];
 
   for (const classListItem of className) {
-    if (classListItem.slice(0, 9) === "language-") {
+    if (classListItem.slice(0, 9) === 'language-') {
       return classListItem.slice(9).toLowerCase();
     }
   }
 
   return null;
+}
+
+function highlight(node: any, lang: string) {
+  // current code components only have 1 child
+  if (!node.children || !node.children.length || !node.children[0].value) {
+    return node;
+  }
+  if (!node.data || !node.data.meta) {
+    return refractor.highlight(toString(node), lang).children;
+  }
+
+  const code = node.children[0].value;
+  // https://regex101.com/r/RWKM9E/1
+  const regex = /(?:[ \t])?\{([^}\s][^}]*)\}/g;
+  const matches = node.data.meta.match(regex);
+  if (!matches || !matches.length) {
+    return refractor.highlight(toString(node), lang).children;
+  }
+
+  const match = matches[0].trim();
+  const linesToHighlight = rangeParser(match.substring(1, match.length - 1));
+
+  const nodes = code.split('\n').reduce(
+    (
+      acc: {
+        type: string;
+        tagName: string;
+        properties?: RefractorElement['properties'];
+        children?: (Text | RefractorElement)[];
+        value?: string;
+      }[],
+      line: string,
+      index: number
+    ) => {
+      const node = {
+        type: 'element',
+        tagName: 'span',
+        properties: {
+          className: ['line', linesToHighlight.includes(index) ? 'line-highlight' : ''],
+        },
+        children: refractor.highlight(line, lang).children,
+      };
+      acc.push(node);
+      acc.push({ type: 'text', tagName: 'code', value: '\n' });
+      return acc;
+    },
+    []
+  );
+
+  return nodes;
 }

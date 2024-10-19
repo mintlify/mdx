296a1b452168cd2772afebd3ddc48fd972ae6eb1
Deep <hello@deeplakhani.co>
2024-10-19 10:29:10 -0400

    add comment

--- src/client/rsc.tsx
+++ src/client/rsc.tsx
@@ -2,6 +2,6 @@ import type { MDXRemoteProps } from 'next-mdx-remote/rsc';
 import { MDXRemote } from 'next-mdx-remote/rsc';
 
 export const MDXServerComponent = ({ source, components }: MDXRemoteProps) => {
-  //@ts-ignore
+  // @ts-ignore: 'MDXRemote' cannot be used as a JSX component.
   return <MDXRemote source={source} components={components} />;
 };

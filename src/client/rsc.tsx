41ebc98eff5e9b7e77bcce48b037d2e3bb19bed5
Deep <hello@deeplakhani.co>
2024-10-19 10:27:52 -0400

    add ts ignore

--- src/client/rsc.tsx
+++ src/client/rsc.tsx
@@ -2,5 +2,6 @@ import type { MDXRemoteProps } from 'next-mdx-remote/rsc';
 import { MDXRemote } from 'next-mdx-remote/rsc';
 
 export const MDXServerComponent = ({ source, components }: MDXRemoteProps) => {
+  //@ts-ignore
   return <MDXRemote source={source} components={components} />;
 };

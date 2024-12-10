a385049ffe6b64b805c936ab45e90cac4087af59
Kaishan (Sam) Ding <kaishan333sam@gmail.com>
2024-12-09 18:52:41 -0800

    remove blade_php

--- src/lib/syntaxHighlighting/blade.js
+++ src/lib/syntaxHighlighting/blade.js
@@ -8,7 +8,6 @@ import refractorMarkup from 'refractor/lang/markup.js';
 import refractorPhp from 'refractor/lang/php.js';
 
 blade.displayName = 'blade';
-blade.aliases = ['blade_php'];
 
 export default function blade(Prism) {
   Prism.register(refractorMarkup);
@@ -18,7 +17,6 @@ export default function blade(Prism) {
     Prism.languages.blade = {
       comment: /{{--([\s\S]*?)--}}/,
 
-      // Blade directives
       directive: {
         pattern: /@\w+(?:::\w+)?(?:\s*\([\s\S]*?\))?/,
         inside: {
@@ -28,7 +26,6 @@ export default function blade(Prism) {
         },
       },
 
-      // Echo statements
       echo: {
         pattern: /\{{2,3}[\s\S]*?\}{2,3}/,
         inside: {
@@ -40,7 +37,6 @@ export default function blade(Prism) {
         },
       },
 
-      // Raw PHP
       php: {
         pattern: /(?:\@php[\s\S]*?\@endphp|\<\?php[\s\S]*?\?\>)/,
         inside: {
@@ -55,27 +51,19 @@ export default function blade(Prism) {
         },
       },
 
-      // HTML markup
       markup: {
         pattern: /<[^?]\/?(.*?)>/,
         inside: Prism.languages.markup,
       },
 
-      // Keywords for common Blade directives
       keyword:
         /\b(?:@if|@else|@elseif|@endif|@foreach|@endforeach|@for|@endfor|@while|@endwhile|@unless|@endunless|@isset|@endisset|@empty|@endempty|@switch|@case|@break|@default|@endswitch|@include|@extends|@section|@endsection|@yield|@stack|@push|@endpush|@auth|@guest|@endauth|@endguest)\b/,
 
-      // Blade variables
       variable: /\$\w+/,
 
-      // Operators
       operator: /=>|->|\|\||&&|!=|==|<=|>=|[+\-*\/%<>]=?|\?:/,
 
-      // Punctuation
       punctuation: /[\[\](){}:;,]/,
     };
-
-    // Add alias for Blade files
-    Prism.languages.blade_php = Prism.languages.blade;
   })(Prism);
 }

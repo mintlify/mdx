fb980f3180a13c16c8673e7d936de60ec8c7bec7
Ronan McCarter <63772591+rpmccarter@users.noreply.github.com>
2024-12-17 15:49:11 -0800

    export SerializeSuccess

--- packages/mdx/src/types/index.ts
+++ packages/mdx/src/types/index.ts
@@ -1,1 +1,5 @@
-export type { SerializeOptions, SerializeResult } from 'next-mdx-remote-client/serialize';
+import type { SerializeOptions, SerializeResult } from 'next-mdx-remote-client/serialize';
+
+type SerializeSuccess = Omit<SerializeResult, 'error'>;
+
+export type { SerializeOptions, SerializeResult, SerializeSuccess };

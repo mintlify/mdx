563f1183e84e61125aab66c7cbd95b9e0fd1ba1b
Kathryn Isabelle Lawrence <lawr3ka@gmail.com>
2025-09-18 10:20:59 -0700

    copy fuma radix popover

--- packages/mdx/src/plugins/rehype/rehypeSyntaxHighlighting.ts
+++ packages/mdx/src/plugins/rehype/rehypeSyntaxHighlighting.ts
@@ -41,7 +41,59 @@ const twoslashOptions: TransformerTwoslashOptions = {
   onShikiError(err, code, lang) {
     console.error(JSON.stringify({ err, code, lang }));
   },
-  renderer: rendererRich(),
+  // copied fuma's approach for custom popup
+  // https://github.com/fuma-nama/fumadocs/blob/dev/packages/twoslash/src/index.ts
+  renderer: rendererRich({
+    hast: {
+      hoverToken: {
+        tagName: 'Popup'
+      },
+      hoverPopup: {
+        tagName: 'PopupContent',
+      },
+      hoverCompose: ({ popup, token}) => [
+        popup,
+        {
+          type: 'element',
+          tagName: 'PopupTrigger',
+          properties: {},
+          children: [token],
+        },
+      ],
+      popupDocs: {
+        class: 'prose twoslash-popup-docs',
+      },
+      popupTypes: {
+        tagName: 'div',
+        class: 'twoslash shiki prose-no-margin',
+        children: (v) => {
+          if (
+            v.length === 1 &&
+            v[0]?.type === 'element' &&
+            v[0]?.tagName === 'pre'
+          )
+            return v;
+
+          return [
+            {
+              type: 'element',
+              tagName: 'code',
+              properties: {
+                class: 'twoslash-popup-code',
+              },
+              children: v,
+            },
+          ];
+        },
+      },
+      popupDocsTags: {
+        class: 'prose twoslash-popup-docs twoslash-popup-docs-tags',
+      },
+      nodesHighlight: {
+        class: 'highlighted-word twoslash-highlighted',
+      },
+    }
+  }),
   langs: ['ts', 'typescript', 'js', 'javascript', 'tsx', 'jsx'],
   explicitTrigger: /mint-twoslash/,
   twoslashOptions: { compilerOptions: twoslashCompilerOptions },
